72183ba429a5dd8ef11cd1a1917b482efd1f2795
UVSQ <poulmanogo@gmail.com>
2016-01-01 16:32:35 +0100

    ajout de la methode isSpare

--- src/test/java/fr/uvsq/coo/ex3_2/BowlingGameTest.java
+++ src/test/java/fr/uvsq/coo/ex3_2/BowlingGameTest.java
@@ -27,11 +27,15 @@ public class BowlingGameTest extends TestCase {
 	}
 	
 	public void testOneSpare(){
-		g.roll(5);
-		g.roll(5); //spare
+		rollSpare(); //spare
 		g.roll(3);
 		rollMany(17,0);
 		assertEquals(16, g.score());
 	}
+	
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);
+		}
 
 }
