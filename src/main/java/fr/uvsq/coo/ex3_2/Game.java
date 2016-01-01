4c62582f731b016939754aaf672c73a952cd5dcc
UVSQ <poulmanogo@gmail.com>
2016-01-01 16:40:29 +0100

    ajout du test testOneStrike

--- src/main/java/fr/uvsq/coo/ex3_2/Game.java
+++ src/main/java/fr/uvsq/coo/ex3_2/Game.java
@@ -14,11 +14,15 @@ public class Game {
 		int score = 0;
 		int frameIndex = 0;
 		for (int frame = 0; frame < 10; frame++){
-			if (isSpare(frameIndex)) // spare
+			if (rolls[frameIndex] == 10) // strike
 			{
-			score += 10 + rolls[frameIndex + 2];
-			frameIndex += 2;
-			} else {
+			score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+			frameIndex ++;
+			} else if (isSpare(frameIndex)){
+				score += 10 + rolls[frameIndex+2];
+				frameIndex += 2;
+			}
+			else {
 				score += rolls[frameIndex] + rolls[frameIndex+1];
 				frameIndex += 2;
 			}
