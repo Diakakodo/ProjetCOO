d4690c115401afb609cf37facca081e06647369a
UVSQ <poulmanogo@gmail.com>
2016-01-03 16:32:48 +0100

    fr.uvsq.coo.ex3_5 : 3. Proposition d'une solution respectant LSP

--- src/main/java/fr/uvsq/coo/ex3_5/Robot.java
+++ src/main/java/fr/uvsq/coo/ex3_5/Robot.java
@@ -3,5 +3,3 @@ package fr.uvsq.coo.ex3_5;
 public abstract class Robot {
 	public abstract void avance();
 }
-
-//1. Cette solution ne respecte pas LSP, car RobotStatique ne respecte plus la sémantique de sa super class Robot
