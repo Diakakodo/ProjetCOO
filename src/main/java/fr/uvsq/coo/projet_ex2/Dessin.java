ba9825c1238771738e02203f9af56ed1f4cd4258
UVSQ <poulmanogo@gmail.com>
2016-01-10 17:51:03 +0100

    definition des methodes d'ajout et de suppression de formes dans un dessin

--- src/main/java/fr/uvsq/coo/projet_ex2/Dessin.java
+++ src/main/java/fr/uvsq/coo/projet_ex2/Dessin.java
@@ -15,16 +15,47 @@ public class Dessin implements Forme {
 	@Override
 	public void chagneCouleurForme(Color nouvelleCouleur) {
 		// TODO Auto-generated method stub
-		
+		//changement de la couleur de toutes les formes du dessin
+		for(Forme forme : listFormes){
+			forme.chagneCouleurForme(nouvelleCouleur);
+		}
 	}
 
 	@Override
 	public double calculSuperficie() {
 		// TODO Auto-generated method stub
-		return 0;
+		//sommation des superficie de toutes les formes du dessin
+		double superficie=0;
+		for(Forme forme : listFormes){
+			superficie+=forme.calculSuperficie();
+		}
+		return superficie;
 	}
 	
-//Generation des getters et setters
+	@Override
+	public void afficher() {
+		// TODO Auto-generated method stub
+		//affichage de toutes les formes du dessin
+		for(Forme forme : listFormes){
+			forme.afficher();
+		}
+	}
+	//Ajouter une forme au dessin
+	public void ajouterForme(Forme nouvelleForme){
+		listFormes.add(nouvelleForme);
+	}
+	//suprimmer une forme du dessin
+	public void supprimeFormeF(Forme F){
+		
+		//suppression de toutes les formes de type F du dessin
+				for(Forme forme : listFormes){
+					if(forme.getClass().equals(F)){
+						listFormes.remove(forme);
+				}
+			}
+	}
+	
+	//Generation des getters et setters
 	public List<Forme> getListFormes() {
 		return listFormes;
 	}
@@ -32,12 +63,5 @@ public class Dessin implements Forme {
 	public void setListFormes(List<Forme> listFormes) {
 		this.listFormes = listFormes;
 	}
-	
-	@Override
-	public void afficher() {
-		// TODO Auto-generated method stub
-
-	}
-	
 
 }
